f615efa00b2263174adefdc19511b3db34c10c55
rakshith <rakshit.s.gowda@gmail.com>
2021-01-07 19:36:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+function App() {
+  const [markdown, setMarkdown] = useState('## markdown preview')
+  return <main>
+    <section className="markdown">
+      <textarea className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)} ></textarea>
+    </section>
+    <article className="result">
+      <ReactMarkdown>
+        {markdown}
+      </ReactMarkdown>
+    </article>
+  </main>
+}
+
+export default App
